803961bc2431cccf07b665dc80577cde07326d57
dwitama jusup <dwitama jusup>
2017-05-10 21:40:48 +0700

    upload awal

---
+++ keranjang.php
@@ -0,0 +1,106 @@
+<?php
+
+include('header.php');
+if(!isset($_SESSION['valid'])) {
+	header('Location: login.php');
+}
+?>
+<?php
+//including the database connection file
+include_once("connection.php");
+
+	$idKeranjang = $_SESSION['idUser'] . session_id();
+	$sessionId = session_id();
+	$idProduk = isset($_POST['idProduk']) ? $_POST['idProduk'] : '';
+	$jumlah = isset($_POST['jumlah']) ? $_POST['jumlah'] : '';
+	$idUser = $_SESSION['idUser'];
+	$total = 0;
+    $p = isset($_GET['aksi']) ? $_GET['aksi'] : '';
+    switch($p) 
+    {
+     default:
+     	echo "<div class='container'>";
+		  echo "<table id=\"keranjang\" cellpadding=\"10\" cellspacing=\"1\">
+		<tr>
+			<th>Nama</th>
+			<th>Jumlah</th>
+			<th>Harga</th>
+			<th>Sub Total</th>
+			<th>Aksi</th>
+		</tr>";
+		         $result = mysqli_query($mysqli, "SELECT * FROM keranjang,detailkeranjang,produk WHERE idUser ='".$idUser."' and produk.idProduk=detailkeranjang.idProduk");
+                 while($dt = mysqli_fetch_array($result, MYSQLI_ASSOC)) {   
+                 	$jumlah = $dt['jumlah'];
+            		
+                        $subTotal = $jumlah * $dt['harga'];
+						$total = $total + $subTotal;
+						$namaProduk = $dt['namaProduk'];
+		echo "<tr>
+				<td>$namaProduk</td>
+				<td>$dt[jumlah]</td>
+				<td>$dt[harga]</td>
+				<td>$subTotal</td>
+				<td><a href=\"delete.php?idProduk=$dt[idProduk]\" onClick=\"return confirm('Anda yakin ingin menghapus?')\">Hapus</a></td>
+			</tr>";
+		}
+              echo "</table>";
+                echo "<h1>$total</h1>";
+                echo "</div>";
+                break;
+     case "tambah" :
+		//insert data to database	
+		$resultSatu = mysqli_query($mysqli, "INSERT INTO keranjang(idKeranjang, idUser, idSession) VALUES('$idKeranjang', '$idUser', '$sessionId')");
+		$cekUdahAda = mysqli_query($mysqli, "SELECT * FROM keranjang,detailkeranjang,produk WHERE idUser ='".$idUser."' and 
+				detailKeranjang.idProduk = ' " . $idProduk . " ' and produk.idProduk=detailkeranjang.idProduk");
+
+			if(mysqli_num_rows($cekUdahAda) < 0) {
+				$result = mysqli_query($mysqli, "INSERT INTO detailkeranjang(idKeranjang, idProduk, jumlah) VALUES ('$idKeranjang','$idProduk', $jumlah)");
+				if($result) {
+				header('Location: keranjang.php');
+				} else {
+					 die('Invalid query: ' . mysql_error());
+				}
+			} else {
+				$query = "UPDATE detailkeranjang SET jumlah=jumlah" . $jumlah . " WHERE idKeranjang='" . $idKeranjang . "'";
+				$result = mysqli_query($mysqli, $query);
+				if($result) {
+				echo '<div style=margin-top:500px>' . $query . '</div>';
+				} else {
+					 die('Invalid query: ' . mysql_error());
+				}
+			}
+		
+			
+			/*
+		echo "<div class='container'>";
+		echo "<table>
+		<th>
+			<td>Nama</td>
+			<td>Jumlah</td>
+			<td>Harga</td>
+			<td>Sub Total</td>
+			<td>Aksi</td>
+		</th>";
+		
+		         $result = mysqli_query($mysqli, "SELECT * FROM keranjang,detailkeranjang,produk WHERE idUser ='".$idUser. '"');
+                 while($dt = mysqli_fetch_array($result, MYSQLI_ASSOC)) {   
+                 	$jumlah = $dt['jumlah'];
+            
+                        $subTotal = $jumlah * $dt['harga'];
+						$total = $total + $subTotal;
+						$namaProduk = $dt['namaProduk'];
+		echo "<tr>
+				<td>$namaProduk</td>
+				<td>$dt[jumlah]</td>
+				<td>$dt[harga]</td>
+				<td>$subTotal</td>
+				<td><a href=\"delete.php?idProduk=$dt[idProduk]\" onClick=\"return confirm('Anda yakin ingin menghapus?')\">Hapus</a></td>
+			</tr>";
+                }
+                echo "</table>";
+                echo "<h1>$total</h1>";
+                echo "</div>";
+                break;*/
+            }
+?>
+</body>
---
+++ w3pomade.sql
@@ -0,0 +1,121 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.16-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping database structure for w3pomade
+DROP DATABASE IF EXISTS `w3pomade`;
+CREATE DATABASE IF NOT EXISTS `w3pomade` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `w3pomade`;
+
+
+-- Dumping structure for table w3pomade.aksesoris
+DROP TABLE IF EXISTS `aksesoris`;
+CREATE TABLE IF NOT EXISTS `aksesoris` (
+  `idAksesoris` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `gambarAksesoris` int(11) NOT NULL,
+  `nama_aksesoris` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idAksesoris`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table w3pomade.aksesoris: ~0 rows (approximately)
+DELETE FROM `aksesoris`;
+/*!40000 ALTER TABLE `aksesoris` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aksesoris` ENABLE KEYS */;
+
+
+-- Dumping structure for table w3pomade.detailkeranjang
+DROP TABLE IF EXISTS `detailkeranjang`;
+CREATE TABLE IF NOT EXISTS `detailkeranjang` (
+  `idKeranjang` int(11) DEFAULT NULL,
+  `idProduks` varchar(50) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  KEY `FK__keranjang` (`idKeranjang`),
+  KEY `FK_detailkeranjang_ikan` (`idProduks`),
+  CONSTRAINT `FK__keranjang` FOREIGN KEY (`idKeranjang`) REFERENCES `keranjang` (`idKeranjang`),
+  CONSTRAINT `FK_detailkeranjang_ikan` FOREIGN KEY (`idProduks`) REFERENCES `produk` (`idProduk`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table w3pomade.detailkeranjang: ~1 rows (approximately)
+DELETE FROM `detailkeranjang`;
+/*!40000 ALTER TABLE `detailkeranjang` DISABLE KEYS */;
+INSERT INTO `detailkeranjang` (`idKeranjang`, `idProduks`, `jumlah`) VALUES
+	(1, 'p002', 2),
+	(1, 'p003', 4);
+/*!40000 ALTER TABLE `detailkeranjang` ENABLE KEYS */;
+
+
+-- Dumping structure for table w3pomade.keranjang
+DROP TABLE IF EXISTS `keranjang`;
+CREATE TABLE IF NOT EXISTS `keranjang` (
+  `idKeranjang` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `idSession` text,
+  PRIMARY KEY (`idKeranjang`),
+  KEY `FK_keranjang_user` (`idUser`),
+  CONSTRAINT `FK_keranjang_user` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table w3pomade.keranjang: ~1 rows (approximately)
+DELETE FROM `keranjang`;
+/*!40000 ALTER TABLE `keranjang` DISABLE KEYS */;
+INSERT INTO `keranjang` (`idKeranjang`, `idUser`, `idSession`) VALUES
+	(1, 1, 'vqac4ar5vfrl00s6407nm6d7k6');
+/*!40000 ALTER TABLE `keranjang` ENABLE KEYS */;
+
+
+-- Dumping structure for table w3pomade.produk
+DROP TABLE IF EXISTS `produk`;
+CREATE TABLE IF NOT EXISTS `produk` (
+  `idProduk` varchar(50) NOT NULL,
+  `namaProduk` varchar(50) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL,
+  `gambarProduk` text,
+  `kategori` char(50) DEFAULT NULL,
+  PRIMARY KEY (`idProduk`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table w3pomade.produk: ~5 rows (approximately)
+DELETE FROM `produk`;
+/*!40000 ALTER TABLE `produk` DISABLE KEYS */;
+INSERT INTO `produk` (`idProduk`, `namaProduk`, `harga`, `stok`, `gambarProduk`, `kategori`) VALUES
+	('p001', 'Murray\'s Original', 80000, 40, 'murraysori.jpg', 'murrays'),
+	('p002', 'Murray\'s Superior V', 100000, 30, 'murrayssuperiorv.jpg', 'murrays'),
+	('p003', 'Murray\'s Super Light', 80000, 20, 'murrayssuperlight.jpg', 'murrays'),
+	('p004', 'Murray\'s Extra Heavy', 120000, 20, 'murraysxtraheavy.jpg', 'murrays'),
+	('p005', 'Murray\'s Small Batch 50-50', 150000, 10, 'smallbatch5050.jpg', 'murrays');
+/*!40000 ALTER TABLE `produk` ENABLE KEYS */;
+
+
+-- Dumping structure for table w3pomade.user
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) DEFAULT NULL,
+  `alamat` varchar(50) DEFAULT NULL,
+  `noTelp` varchar(50) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idUser`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table w3pomade.user: ~2 rows (approximately)
+DELETE FROM `user`;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`idUser`, `nama`, `alamat`, `noTelp`, `username`, `password`) VALUES
+	(1, 'debi', 'antapani', '087718938581', 'dwittamma', 'ce60e43f0b1ba31f8f8ee41275dd9224'),
+	(2, 'ewock', 'antafunky', '08787878787', 'ewock', '07664678c3d89d2da2e2d740ae4426fa');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
